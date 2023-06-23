e1b7323ed6971f981bf62c30c6801dc869185782
Pkanand1999 <singhisking810267@gmail.com>
2023-06-23 17:53:12 +0530

    front

--- client/goldtech/src/Components/Nav.jsx
+++ client/goldtech/src/Components/Nav.jsx
@@ -1,19 +1,16 @@
 import React from 'react'
 import {
-    Table,
-    Thead,
-    Tbody,
-    Tfoot,
-    Tr,
-    Th,
-    Td,
-    TableCaption,
-    TableContainer,
-  } from '@chakra-ui/react'
+    HStack,
+    Heading,
+} from '@chakra-ui/react'
+import {Link} from 'react-router-dom'
 
-export default function Nav() {
-  return (
-    <>
-  </>
-  )
+ function Nav() {
+    return (
+            <HStack  display="flex" width="100%" justifyContent="center" color="black"  alignItem="center" bg="grey" positon="fixed">
+                    <Heading display="flex" fontSize="40px" padding="0"><Link to="/"> Gold Tech</Link></Heading>
+            </HStack>
+    )
 }
+
+export default Nav
--- client/goldtech/src/pages/Dashboard.jsx
+++ client/goldtech/src/pages/Dashboard.jsx
@@ -3,13 +3,16 @@ import {
     Table,
     Thead,
     Tbody,
-    Tfoot,
     Tr,
     Th,
     Td,
     TableCaption,
     TableContainer,
+    Box,
+    Button,
+    Modal,
   } from '@chakra-ui/react'
+  import {Link} from 'react-router-dom'
 
 export default function Dashboard() {
     const [data,setData]=useState([])
@@ -22,8 +25,9 @@ export default function Dashboard() {
 
 async function fetching(page) {
   try{
-    let res=await fetch(`http://localhost:8080/public/v2/users?page=${page}&limit=10`)
+    let res=await fetch(`http://localhost:8080/users/public/v2/get?page=${page}&limit=10`)
     let json=await res.json()
+    setData([...json])
   }catch(e){
     console.log(e)
   }
@@ -31,25 +35,38 @@ async function fetching(page) {
 
 
   return ( <>
-      <TableContainer width="ifr" display="flex" justifyContent="center" marginTop="10%">
+      <TableContainer width="ifr" display="flex" justifyContent="center" marginTop="4%">
   <Table variant='simple' width="95%" >
-    <TableCaption>user credentials</TableCaption>
-    <Thead>
-      <Tr>
-        <Th>To convert</Th>
-        <Th>into</Th>
-        <Th isNumeric>multiply by</Th>
+    <TableCaption bg="teal"  alignItem="center" justifyContent="center" flexDirection="column">user credentials</TableCaption>
+    <Thead bg="blue" padding="2%">
+      <Tr textAlign="center" padding="2%">
+        <Th padding="1% 1%">S.no</Th>
+        <Th>Name</Th>
+        <Th >Email</Th>
+        <Th >Updata</Th>
+        <Th >View</Th>
       </Tr>
     </Thead>
-    <Tbody>
-      <Tr>
-        <Td>inches</Td>
-        <Td>millimetres (mm)</Td>
-        <Td isNumeric>25.4</Td>
-      </Tr>
+    <Tbody >
+        {data.map((data,ind)=>{
+            return <Tr textAlign="center" border="4px solid green" bg="pink" Key={ind}>
+            <Td padding="2% 1%">{(ind+1)+(10*page-1)-10+1}</Td>
+            <Td>{data.name}</Td>
+            <Td >{data.email}</Td>
+            <Td bg="grey" ><Link to={`/editdetails/${data.id}`}>Edit</Link></Td>
+            <Td bg="lightGreen"><Link to={`/userdetails/${data.id}`}>view</Link></Td>
+          </Tr>
+        })}
+      
     </Tbody>
   </Table>
 </TableContainer>
+
+<Box display="flex"  margin="5% 50%" justifyContent="center" alignItem="center">
+    <Button marginRight="20px" borderRadius="5px" padding="10px" border="none" isDisabled={page===1? true:false} onClick={()=>setPage(page-1)}>←Pre</Button> {page} 
+    <Button borderRadius="10px" padding="10px"  marginLeft="20px" border="none" isDisabled={page===10? true:false} onClick={()=>setPage(page+1)}>Next→</Button>
+</Box>
 </>
   )
 }
+
